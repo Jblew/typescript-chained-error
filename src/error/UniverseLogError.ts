017e0af0e95e149e5c25e8c2ace81bc4d5ca3982
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-23 12:41:24 +0200

    Extract chained error from universe-log library

--- src/error/UniverseLogError.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from "./CustomError";
-
-export class UniverseLogError extends CustomError {
-    public constructor(message?: string, cause?: Error) {
-        super(message, cause);
-    }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export { AbstractUniverseLog } from "./AbstractUniverseLog";
-export { LogMetadata } from "./config/LogMetadata";
-export { LogLevel } from "./config/LogLevel";
-export { PortableEnv } from "./config/PortableEnv";
-export { CustomError } from "./error/CustomError";
-export { UniverseLogError } from "./error/UniverseLogError";
-export { LogFormat } from "./format/LogFormat";
+export { ChainedError } from "./ChainedError";
